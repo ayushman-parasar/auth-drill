78d8e949ce8ecae2765ae959938d9263b70f1e79
Ayush_Mann <learnertsk@gmail.com>
2020-04-24 23:54:16 +0530

    identification route completed

--- routes/api/v1/identify.js
+++ routes/api/v1/identify.js
@@ -2,19 +2,21 @@ const express = require("express");
 const router = express.Router();
 const methods = require('../../../modules/methods')
 const auth = require('../../../modules/auth')
-const Student = require("../../../models/Mentor")
+const Student = require("../../../models/Students")
 const Mentor = require("../../../models/Mentor")
 
 router.get('/',auth.verifyToken,methods.checkMentor,async function(req, res, next){
     let id  = req.user.userID
     console.log(req.user)
-    // if(req.user.mentorChecked == false){
-    //     const student = await Student.findById(id)
-    //     res.json({student})
-    // }else{
-    //     const mentor = await  Mentor.findById(id)
-    //     res.json(mentor)
-    // } 
+    if(req.user.mentorChecked == false){
+        console.log("inside")
+        const student = await Student.findById(id)
+        console.log(student)
+        res.json({student})
+    }else{
+        const mentor = await  Mentor.findById(id)
+        res.json(mentor)
+    } 
     
 })
 
